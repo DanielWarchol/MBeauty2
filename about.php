6e33027e904d9ca5efb35f0b7730dd8cfd61be71
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2022-01-12 16:37:03 +0100

    mbeauty

---
+++ about.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>MBeauty_Poprawa</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/lakiery.css">
+    <!-- Link PHP -->
+    <link rel="stylesheet" href="index.php">
+    <link rel="stylesheet" href="lakiery.php">
+    <link rel="stylesheet" href="rzesy.php">
+    <link rel="stylesheet" href="akcesoria.php">
+    <link rel="stylesheet" href="about.php">
+    <!-- Link-1 PHP -->
+    <link rel="stylesheet" href="lakiery-1.php">
+    <!-- Link-1 CSS -->
+    <link rel="stylesheet" href="css/lakiery-1.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <!-- Navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-light  py-3 fixed-top">
+        <div class="container-fluid">
+            <img src="img/MBeauty-2-1.png" alt="">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="lakiery.php">Lakiery</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="rzesy.php">Rzęsy</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="akcesoria.php">Akcesoria</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="about.php">O Nas</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="contact.php">Kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="shopping-cart.php"><i class="fas fa-shopping-bag"></i></a>
+                        <span id="counter">0</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar end -->
+
+
+    <!-- About start -->
+    <section id="about" class=" container my-5 py-5">
+      <div class="container text-center mt-5 py-5">
+        <h3>O nas</h3>
+        <hr class="mx-auto">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat natus illum dicta, dolore inventore quos autem in voluptates ea minus consequuntur quod suscipit fugit ipsa ab quam quo? Ut doloremque delectus alias laborum quis amet architecto totam, quos maiores dolorem!</p>
+      </div>
+    </section>
+    <!-- About end -->
+
+    
+    <!-- Footer start -->
+    <footer class="mt-5 py-5">
+      <div class="row container mx-auto pt-5">
+        <div class="footer-one col-lg-3 col-md-6 col-12">
+          <img src="img/MBeauty-2-1.png" alt="">
+          <p class="pt-3">Fringilla urna porttitor rhoncus dolor purus luctus venenatis lectus magna fringilla diam maecenas ultricies mi eget mauris.</p>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
+          <h5 class="pb-2">Menu</h5>
+          <ul class="text-uppercase list-unstyled">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="akcesoria.php">Akcesoria</a></li>
+            <li><a href="about.php">O nas</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
+          <h5 class="pb-2">Kontakt</h5>
+          <div>
+            <h6 class="text-uppercase">Address</h6>
+            <p>123 STREET NAME, CITY, US</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Phone</h6>
+            <p>(123) 456-7890</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Email</h6>
+            <p>MAIL@EXAMPLE.COM</p>
+          </div>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-12">
+          <h5 class="pb-2">Instagram</h5>
+          <div class="row">
+            <img class="img-fluid w-25 h-100 m-2" src="img/insta/1.jpg" alt="">
+            <img class="img-fluid w-25 h-100 m-2" src="img/insta/2.jpg" alt="">
+            <img class="img-fluid w-25 h-100 m-2" src="img/insta/3.jpg" alt="">
+            <img class="img-fluid w-25 h-100 m-2" src="img/insta/4.jpg" alt="">
+            <img class="img-fluid w-25 h-100 m-2" src="img/insta/5.jpg" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="copyright mt-5">
+        <div class="row container mx-auto">
+          <div class="col-lg-3 col-md-6 col-12 mb-4">
+            <img src="img/payment.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 col-12 text-nowrap mb-3">
+            <p>MBeauty E-commerce &copy; 2022. By DanielWeb</p>
+          </div>
+          <div class="col-lg-4 col-md-6 col-12">
+            <a href="https://pl-pl.facebook.com"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+            <a href="https://accounts.snapchat.com"><i class="fab fa-snapchat-ghost"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer end -->
+
+    <!-- Optional JavaScript -->
+    <script src="js/navbar-stick.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
